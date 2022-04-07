b23b9988389af1f5dc006da5955a822ad2f5eb6c
Yeong <64449625+Syeong0013@users.noreply.github.com>
2022-04-07 10:17:15 +0900

    Delete src directory

--- src/pkg/Scanner/Scanner.java
+++
@@ -1,5 +0,0 @@
-package Pkg.Scanner;
-
-public class Scanner {
-
-}
--- src/pkg/StaticFinal/StaticFinalExec.java
+++
@@ -1,8 +0,0 @@
-package Pkg.StaticFinal;
-
-public class StaticFinalExec {
-	public static void main(String[] args) {
-		System.out.println("파이 (소수점 5자리까지) : " + StaticFinal.PI);
-		// 파이 (소수점 5자리까지) : 3.14159
-	}
-}
