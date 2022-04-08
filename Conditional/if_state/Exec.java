c243c02d94b876e8b5d8a297139fb46c94d03066
Yeong <64449625+Syeong0013@users.noreply.github.com>
2022-04-08 12:14:48 +0900

    Update Exec.java

--- Conditional/if_state/Exec.java
+++ Conditional/if_state/Exec.java
@@ -5,7 +5,7 @@ public class Exec {
 		Product product1 = new Product(3000);
 		Product product2 = new Product(12000);
 		
-		product1.Pick();
-		product2.Pick();
+		product1.Pick();	// 출력 : 구매합니다.
+		product2.Pick();	// 출력 : 구매하지않습니다.
 	}
 }
