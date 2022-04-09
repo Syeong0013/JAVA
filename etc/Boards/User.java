6111b97d0d615587f206c9f8d12e3360c72258e1
Yeong <64449625+Syeong0013@users.noreply.github.com>
2022-04-09 16:14:06 +0900

    Create User.java

---
+++ etc/Boards/User.java
@@ -0,0 +1,26 @@
+
+public class User {
+
+	public String userName;
+	public String userId;
+	
+	public User() {
+		
+	}
+	
+	public User(String userName, String userId) {
+		this.userName = userName;
+		this.userId = userId;
+	}
+	
+	
+	// 글을 작성하는 행위 메소드 
+	public void write(User user, Post post, Boards postList) {
+		
+		post.insert(user);
+		
+		postList.addPost(post);
+		postList.showPostList();
+		
+	}
+}
