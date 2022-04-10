4b7d35259943ae74ff7606d913a845c31377c11a
Yeong <64449625+Syeong0013@users.noreply.github.com>
2022-04-10 17:07:25 +0900

    글 번호 자동 생성 기능 추가

--- etc/Boards/Post.java
+++ etc/Boards/Post.java
@@ -17,14 +17,19 @@ public class Post {
 		
 		
 	// 행위 메소드 
-	public void insert(User user) {		
+	public void insert(User user, int regnum) {		
 		Scanner sc = new Scanner(System.in);
-			
+		
+		/*
 		System.out.println("글 번호를 입력하시오. ");
 		this.regnum = sc.nextInt();
 		// 개행문자 삭제 
 		sc.nextLine();
-			
+		*/
+		
+		//글 번호 자동 생성 기능
+		this.regnum = regnum;
+		
 		System.out.println("글 제목을 입력하시오. ");
 		this.strTitle = sc.nextLine();
 			
