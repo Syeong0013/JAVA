4b085be6557c156debc58a05f5308fd490b29212
Yeong <64449625+Syeong0013@users.noreply.github.com>
2022-04-10 17:08:24 +0900

    글 번호 자동 생성 기능 추가

--- etc/Boards/User.java
+++ etc/Boards/User.java
@@ -17,7 +17,7 @@ public class User {
 	// 글을 작성하는 행위 메소드 
 	public void write(User user, Post post, Boards postList) {
 		
-		post.insert(user);
+		post.insert(user, postList.makeRegnum());
 		
 		postList.addPost(post);
 		postList.showPostList();
