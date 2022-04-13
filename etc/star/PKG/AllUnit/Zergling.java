500800d7dc714f78e7fc9d0ec01dbc62adcbdf23
Yeong <64449625+Syeong0013@users.noreply.github.com>
2022-04-13 15:07:21 +0900

    Create Zergling.java

---
+++ etc/star/PKG/AllUnit/Zergling.java
@@ -0,0 +1,57 @@
+package Pkg.AllUnit;
+
+import Pkg.Commons.*;
+
+public class Zergling extends Unit implements IAttack{
+	
+	private int[] point = {1,2,3,5,6};
+	
+	// 생성자
+	public Zergling() {
+		super();
+		this.offense = 2;
+		this.health = 10;
+		this.killPoint = 2;
+		this.unitkind = UnitKind.Zerg;
+	}
+	
+	// 추상클래스 추상메소드 재정의
+	@Override
+	public void upgrade() {
+		this.level ++;
+		System.out.println("축하합니다.");
+		System.out.println(this.name + "레벨이 업그레이드 되었습니다.");
+		System.out.println(this.name + "의 현재 레벨 : "+ Integer.toString(this.level));
+		System.out.println(this.name + "의 현재 체력 : " + Integer.toString(this.health + (point[level-1] * 2)));
+		System.out.println(this.name + "의 현재 공격력 : " + Integer.toString((this.offense) + (point[level-1])));
+		this.health += point[level-1] * 2;
+		this.offense += point[level-1];
+		this.killPoint += this.killPoint;
+	}
+	
+	// 인터페이스 추상메소드 재정의
+	@Override
+	public void attack(AllUnit unit) {
+		System.out.println("----------------------------");
+		if(this.chkState(unit)) {
+			if(unit instanceof IFly){
+				System.out.println("\t*** 공격불가 ***");
+				System.out.println(this.name + "은 날아다니는 " + unit.getName() + "을 공격할 수 없습니다.");
+				System.out.println("----------------------------");
+			}
+			else {
+				System.out.println(this.name + "이 " + unit.getName() + "을 공격합니다.");
+				unit.attacked(this.offense);
+				if(unit.state == State.die) {
+					this.killNum ++;
+					if(this.killPoint == this.killNum) {
+						this.upgrade();
+					}
+				}
+			}
+		}
+	}
+}	
+
+
+
