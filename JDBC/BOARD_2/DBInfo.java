de92ed72be23ba3e303c92a36c0f7b5cf24c7994
Yeong <64449625+Syeong0013@users.noreply.github.com>
2022-04-18 15:35:48 +0900

    Create DBInfo.java

---
+++ JDBC/BOARD_2/DBInfo.java
@@ -0,0 +1,9 @@
+package Pkg.DB;
+
+public class DBInfo {
+
+	public static String DBID = "c##Board";
+	public static String DBPW = "1";;
+	public static String DBUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+	
+}
